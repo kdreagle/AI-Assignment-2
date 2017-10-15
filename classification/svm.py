527b67a3fea262e28e73856b702efefdfcf2429f
kdreagle <kdreagle@gmail.com>
2017-10-15 13:36:20 -0400

    I think the SVM is finished
    Works with about 80% accuracy, trying to figure out how to make it more accurate.

--- classification/svm.py
+++ classification/svm.py
@@ -3,28 +3,42 @@
 
 # svm implementation
 import util
+import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsRestClassifier
 PRINT = True
 
 class SVMClassifier:
-  """
-  svm classifier
-  """
-  def __init__( self, legalLabels):
-    self.legalLabels = legalLabels
-    self.type = "svm"
-      
-  def train( self, trainingData, trainingLabels, validationData, validationLabels ):
-    print "Starting iteration ", iteration, "..."
-    for i in range(len(trainingData)):
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-    
-  def classify(self, data ):
-    guesses = []
-    for datum in data:
-      # fill predictions in the guesses list
-      "*** YOUR CODE HERE ***"
-      util.raiseNotDefined()
-      
-    return guesses
+    """
+    svm classifier
+    """
+    def __init__( self, legalLabels):
+        self.legalLabels = legalLabels
+        self.type = "svm"
+        self.svm = OneVsRestClassifier(LinearSVC())
 
+    def train( self, trainingData, trainingLabels, validationData, validationLabels ):
+        iteration = 0
+        print "Starting iteration ", iteration, "..."
+        X = []
+        y = []
+
+        for i in range(len(trainingData)):
+            X.append([])
+            y.append(trainingLabels[i])
+            for point in trainingData[i]:
+                X[i].append(trainingData[i][point])
+
+        self.svm.fit(X,y)
+
+    def classify(self, data):
+        
+        X = []
+        for i in range(len(data)):
+            X.append([])
+            for point in data[i]:
+                X[i].append(data[i][point])
+
+        guesses = self.svm.predict(X)
+
+        return guesses
